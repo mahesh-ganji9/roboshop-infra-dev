424c2077846c77249e0af7c9d4a64ee52291621c
mahesh <mahesh050917@gmail.com>
2026-03-06 20:01:49 +0530

    terraform vpc modules

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -9,32 +9,32 @@ resource "aws_instance" "mongodb" {
 }
 
 
-resource "aws_instance" "redis" {
-  ami                    = local.ami_id
-  instance_type          = var.instance_type
-  subnet_id              = local.db_private_subnet_ids
-  vpc_security_group_ids = local.redissg_id
-  tags = merge(local.common_tags, {
-    Name = "${var.project}-${var.env}-redis" }
-  )
-}
+# resource "aws_instance" "redis" {
+#   ami                    = local.ami_id
+#   instance_type          = var.instance_type
+#   subnet_id              = local.db_private_subnet_ids
+#   vpc_security_group_ids = local.redissg_id
+#   tags = merge(local.common_tags, {
+#     Name = "${var.project}-${var.env}-redis" }
+#   )
+# }
 
-resource "aws_instance" "mysql" {
-  ami                    = local.ami_id
-  instance_type          = var.instance_type
-  subnet_id              = local.db_private_subnet_ids
-  vpc_security_group_ids = local.mysqlsg_id
-  tags = merge(local.common_tags, {
-    Name = "${var.project}-${var.env}-mysql" }
-  )
-}
+# resource "aws_instance" "mysql" {
+#   ami                    = local.ami_id
+#   instance_type          = var.instance_type
+#   subnet_id              = local.db_private_subnet_ids
+#   vpc_security_group_ids = local.mysqlsg_id
+#   tags = merge(local.common_tags, {
+#     Name = "${var.project}-${var.env}-mysql" }
+#   )
+# }
 
-resource "aws_instance" "rabbitmq" {
-  ami                    = local.ami_id
-  instance_type          = var.instance_type
-  subnet_id              = local.db_private_subnet_ids
-  vpc_security_group_ids = local.rabbitmqsg_id
-  tags = merge(local.common_tags, {
-    Name = "${var.project}-${var.env}-rabbitmq" }
-  )
-} 
+# resource "aws_instance" "rabbitmq" {
+#   ami                    = local.ami_id
+#   instance_type          = var.instance_type
+#   subnet_id              = local.db_private_subnet_ids
+#   vpc_security_group_ids = local.rabbitmqsg_id
+#   tags = merge(local.common_tags, {
+#     Name = "${var.project}-${var.env}-rabbitmq" }
+#   )
+# } 
--- 40-databases/rt53.tf
+++ 40-databases/rt53.tf
@@ -9,29 +9,29 @@ resource "aws_route53_record" "mongodb_record" {
   name            = "mongodb-${var.env}.${var.domain_name}"
 }
 
-resource "aws_route53_record" "redis_record" {
-  zone_id         = var.zone_id
-  records         = [aws_instance.redis.private_ip]
-  type            = "A"
-  ttl             = 1
-  allow_overwrite = true
-  name            = "redis-${var.env}.${var.domain_name}"
-}
+# resource "aws_route53_record" "redis_record" {
+#   zone_id         = var.zone_id
+#   records         = [aws_instance.redis.private_ip]
+#   type            = "A"
+#   ttl             = 1
+#   allow_overwrite = true
+#   name            = "redis-${var.env}.${var.domain_name}"
+# }
 
-resource "aws_route53_record" "mysql_record" {
-  zone_id         = var.zone_id
-  records         = [aws_instance.mysql.private_ip]
-  type            = "A"
-  ttl             = 1
-  allow_overwrite = true
-  name            = "mysql-${var.env}.${var.domain_name}"
-}
+# resource "aws_route53_record" "mysql_record" {
+#   zone_id         = var.zone_id
+#   records         = [aws_instance.mysql.private_ip]
+#   type            = "A"
+#   ttl             = 1
+#   allow_overwrite = true
+#   name            = "mysql-${var.env}.${var.domain_name}"
+# }
 
-resource "aws_route53_record" "rabbitmq_record" {
-  zone_id         = var.zone_id
-  records         = [aws_instance.rabbit.private_ip]
-  type            = "A"
-  ttl             = 1
-  allow_overwrite = true
-  name            = "rabbitmq-${var.env}.${var.domain_name}"
-}
+# resource "aws_route53_record" "rabbitmq_record" {
+#   zone_id         = var.zone_id
+#   records         = [aws_instance.rabbit.private_ip]
+#   type            = "A"
+#   ttl             = 1
+#   allow_overwrite = true
+#   name            = "rabbitmq-${var.env}.${var.domain_name}"
+# }
