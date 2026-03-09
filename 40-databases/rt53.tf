4649b874c5ca8778c18d024f858c80583596f27d
mahesh <mahesh050917@gmail.com>
2026-03-09 21:19:26 +0530

    updated terraform code

--- 40-databases/rt53.tf
+++ 40-databases/rt53.tf
@@ -9,14 +9,14 @@ resource "aws_route53_record" "mongodb_record" {
   name            = "mongodb-${var.env}.${var.domain_name}"
 }
 
-resource "aws_route53_record" "redis_record" {
-  zone_id         = var.zone_id
-  records         = [aws_instance.redis.private_ip]
-  type            = "A"
-  ttl             = 1
-  allow_overwrite = true
-  name            = "redis-${var.env}.${var.domain_name}"
-}
+# resource "aws_route53_record" "redis_record" {
+#   zone_id         = var.zone_id
+#   records         = [aws_instance.redis.private_ip]
+#   type            = "A"
+#   ttl             = 1
+#   allow_overwrite = true
+#   name            = "redis-${var.env}.${var.domain_name}"
+# }
 
 resource "aws_route53_record" "mysql_record" {
   zone_id         = var.zone_id
@@ -27,11 +27,11 @@ resource "aws_route53_record" "mysql_record" {
   name            = "mysql-${var.env}.${var.domain_name}"
 }
 
-resource "aws_route53_record" "rabbitmq_record" {
-  zone_id         = var.zone_id
-  records         = [aws_instance.rabbitmq.private_ip]
-  type            = "A"
-  ttl             = 1
-  allow_overwrite = true
-  name            = "rabbitmq-${var.env}.${var.domain_name}"
-}
+# resource "aws_route53_record" "rabbitmq_record" {
+#   zone_id         = var.zone_id
+#   records         = [aws_instance.rabbitmq.private_ip]
+#   type            = "A"
+#   ttl             = 1
+#   allow_overwrite = true
+#   name            = "rabbitmq-${var.env}.${var.domain_name}"
+# }
