9968f507113d99eee3df8dacae492e07979a7a4d
mahesh <mahesh050917@gmail.com>
2026-03-10 09:45:50 +0530

    updated terraform code

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -2,8 +2,8 @@ locals {
   ami_id = data.aws_ami.ami_data.id
   # public_subnet_id = split(",",data.aws_ssm_parameter.public_subnet_ids.value)[0]
   db_private_subnet_ids = split(",", data.aws_ssm_parameter.db_private_subnet_ids.value)[0]
-  mysql_role_name = join("-", [for name in ["${var.project}","${var.env}",mysql-role]: title(name)])
-  mysql_role_policy = join("-", [for name in ["${var.project}","${var.env}",mysql-policy]: title(name)])
+  mysql_role_name = join("-", [for name in ["${var.project}","${var.env}",mysqlrole]: title(name)])
+  mysql_role_policy = join("-", [for name in ["${var.project}","${var.env}",mysqlpolicy]: title(name)])
   mongodbsg_id          = data.aws_ssm_parameter.mongosg.value
   redissg_id            = data.aws_ssm_parameter.redis.value
   mysqlsg_id            = data.aws_ssm_parameter.mysql.value
