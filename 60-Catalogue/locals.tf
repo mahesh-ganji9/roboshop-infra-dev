ab917e206691251bfde32aaa390be7696e673cbf
mahesh <mahesh050917@gmail.com>
2026-03-12 10:53:19 +0530

    update terraform code

--- 60-Catalogue/locals.tf
+++ 60-Catalogue/locals.tf
@@ -1,6 +1,6 @@
 locals {
   ami_id = data.aws_ami.ami_data.id
-  catalogue_sg_id = data.aws_ssm_parameter.Catalogue_sg_id
+  catalogue_sg_id = data.aws_ssm_parameter.Catalogue_sg_id.value
   private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
   common_tags = {
     project   = var.project
