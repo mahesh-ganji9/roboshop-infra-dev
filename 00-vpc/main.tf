2a0ec1bb072aab58285bd9fd0dd27b5beb5327e0
Mahesh Ganji <mahesh9.storage@gmail.com>
2026-03-20 13:15:12 +0530

    update terraform code

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source                  = "../../terraform-aws-vpc"
+  source                  = "git::https://github.com/mahesh-ganji9/terraform-aws-vpc.git?ref=main"
   cidr_block              = "10.0.0.0/16"
   private_subnet_cidrs    = ["10.0.11.0/24", "10.0.12.0/24"]
   db_private_subnet_cidrs = ["10.0.21.0/24", "10.0.22.0/24"]
